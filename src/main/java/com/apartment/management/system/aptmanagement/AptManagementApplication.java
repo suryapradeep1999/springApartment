b7fec7da80788419153bd28f4748da2029fcff31
suryapradeep1999 <suryapradeep97@gmail.com>
2023-11-19 21:01:25 +0530

    intial commit

---
+++ src/main/java/com/apartment/management/system/aptmanagement/AptManagementApplication.java
@@ -0,0 +1,13 @@
+package com.apartment.management.system.aptmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AptManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AptManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apartment/management/system/aptmanagement/AptManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apartment.management.system.aptmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AptManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
